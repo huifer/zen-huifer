d2a73f06a6985c4491ff0863b192b99719728785
Zen Huifer <huifer97@163.com>
2024-06-27 16:20:33 +0800

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,25 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
+import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
+	output: 'server',
+	adapter: vercel({
+		webAnalytics: { enabled: true }
+	}),
+
+	site: "https://zen-huifer.vercel.app/",
 	integrations: [
 		starlight({
+			head: [
+				{
+					tag: "meta",
+					attrs:{
+						name: "google-adsense-account",
+						content:"ca-pub-2032930149616773"
+					}
+				}
+			],
 			title: 'Zen HuiFer Doc',
 			social: {
 				github: 'https://github.com/withastro/starlight',
--- src/content/docs/zh-cn/ai/why-rag.md
+++
@@ -1,5 +0,0 @@
----
-title: 为什么都是RAG？
-description: A guide in my new Starlight docs site.
----
-
